d60bd30dec56a2cbe16326b52297c8538e900e57
Isuru Haththotuwa <isuruh@apache.org>
2016-03-10 12:30:21 +0530

    supporting other profiles than default

--- test.sh
+++ test.sh
@@ -39,7 +39,6 @@
 set -e
 
 kubernetes_artifact_version=1.0.0
-default_profile=default
 source environment.bash
 source ~/.bash_profile
 
@@ -53,7 +52,7 @@ user=`whoami`
 echo "user executing the script $user"
 
 # products to be tested
-products=(wso2dss,3.5.0)
+products=(wso2dss,3.5.0,manager wso2dss,3.5.0,worker)
 declare -A results
 
 function echoError {
@@ -81,12 +80,11 @@ function build_base_image {
 function build_docker_image_and_scp {
     # switch to IS 5 docker directory and build
     pushd ${root_dir}/$1/docker > /dev/null
-    echo pwd=`pwd`
-    sudo --preserve-env bash build.sh $2 ${kubernetes_artifact_version} $3 || print_results
+    sudo --preserve-env bash build.sh "$2" ${kubernetes_artifact_version} "$3" || cleanup
     # save the image as a tar file
-    sudo bash save.sh $2 ${kubernetes_artifact_version} $3 || print_results
+    sudo bash save.sh "$2" ${kubernetes_artifact_version} "$3" || cleanup
     sudo chown ${user}:${user} -R ~/docker/
-    bash scp.sh ${KUBERNETES_NODE_USER}@${KUBERNETES_NODE} $2 ${kubernetes_artifact_version} $3 || print_results
+    bash scp.sh ${KUBERNETES_NODE_USER}@${KUBERNETES_NODE} "$2" ${kubernetes_artifact_version} "$3" || cleanup
     popd > /dev/null
 }
 
@@ -97,14 +95,14 @@ function undeploy_kubernetes_artifacts {
     popd > /dev/null
 }
 
-function deploy_kubernetes_artifacts {
+function deploy_kubernetes_service {
     # switch to IS 5 kubernetes directory deploy kubernetes artifacts
-    pushd ${root_dir}/$1/kubernetes > /dev/null
-    echo "Deploying $1 $2 service..."
-    kubectl create -f "$1"-"$2"-service.yaml || print_results
-    echo "Deploying $1 $2 controller..."
-    kubectl create -f "$1"-"$2"-controller.yaml || print_results
-    popd > /dev/null
+    bash ${common_dir}/scripts/kubernetes/deploy-kubernetes-service.sh "$1" "$2" || cleanup
+}
+
+function deploy_kubernetes_rc {
+    # switch to IS 5 kubernetes directory deploy kubernetes artifacts
+    bash ${common_dir}/scripts/kubernetes/deploy-kubernetes-rc.sh "$1" "$2" || cleanup
 }
 
 
@@ -113,7 +111,7 @@ function check_status {
     success_count=0
     while true; do
         # check the pod status
-        result=`kubectl get pods | grep $1`
+        result=`kubectl get pods | grep $1-$3`
         if [[ -z "$result" ]]; then
            echo "no pod found for product $1"
            error_count=$((error_count + 1))
@@ -136,7 +134,7 @@ function check_status {
         if [[ ${error_count} -gt 5 ]]; then
             echo "error condition threshold reached: $error_count, aborting"
 	        #undeploy_kubernetes_artifacts "$1"
-            results[$1-$2]="ERROR, last pod name=$pod_name, state=$state, restarts=$restarts"
+            results[$1-$2-$3]="ERROR, last pod name=$pod_name, state=$state, restarts=$restarts"
             break
         elif [[ ${success_count} -gt 5 ]]; then
             echo "success condition threshold reached: $success_count"
@@ -144,13 +142,13 @@ function check_status {
             if [[ ${server_started} -eq 0 ]]; then
                 msg="Carbon Server $1-$2 in pod $pod_name has started successfully"
                 echo ${msg}
-                results[$1-$2]="SUCCESS, $msg"
+                results[$1-$2-$3]="SUCCESS, $msg"
                 break
             else
                 msg="Carbon Server $1-$2 in pod $pod_name has failed to start"
                 echo ${msg}
                 #undeploy_kubernetes_artifacts "$1"
-                results[$1-$2]="ERROR, $msg"
+                results[$1-$2-$3]="ERROR, $msg"
                 break
             fi
         else
@@ -194,23 +192,37 @@ function validate_environment {
 }
 
 function test {
+    # build and scp
+#    for product in ${products[@]}; do
+#        IFS=","
+#        set ${product}
+#        echo "building docker image for=$1  version=$2 profile=$3"
+#        build_docker_image_and_scp "$1" "$2" "$3"
+#        unset IFS
+#    done
+    # deploy all Services
     for product in ${products[@]}; do
         IFS=","
         set ${product}
-        echo "testing $1 v.$2"
-        echo 'building docker image for='$1 ' version='$2
-        build_docker_image_and_scp "$1" "$2" "${default_profile}"
-        echo 'deploying kubernetes artifacts for='$1 ' version='$2
-        deploy_kubernetes_artifacts "$1" "${default_profile}"
-        check_status "$1" "$2"
-        echo 'undeploying kubernetes artifacts for='$1 ' version='$2
-        undeploy_kubernetes_artifacts "$1"
-        echo "completed testing $1 v.$2"
+        echo "deploying kubernetes Service for=$1 version=$2 profile=$3"
+        deploy_kubernetes_service "$1" "$3"
+        unset IFS
+    done
+    # deploy RCs and check status
+    for product in ${products[@]}; do
+        IFS=","
+        set ${product}
+        echo "deploying kubernetes RC for=$1 version=$2 profile=$3"
+        deploy_kubernetes_rc "$1" "$3"
+        check_status "$1" "$2" "$3"
+#        echo "undeploying kubernetes artifacts for=$1 version=$2 profile=$3"
+#        undeploy_kubernetes_artifacts "$1"
         unset IFS
     done
 }
 
 function print_results {
+    unset IFS
     echoBold "################################################# Results #################################################"
     for product in ${products[@]}; do
         IFS=","
@@ -224,6 +236,17 @@ function print_results {
         unset IFS
     done
     echoBold "############################################## End of Results #############################################"
+}
+
+function cleanup {
+    unset IFS
+    for product in ${products[@]}; do
+        IFS=","
+        set ${product}
+        echo "Cleaning up for=$1 version=$2 profile=$3"
+        undeploy_kubernetes_artifacts "$1"
+        unset IFS
+    done
 }
 
 # validate
